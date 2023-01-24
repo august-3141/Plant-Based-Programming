8093398c697606d6b2032cfbcdad7cbc6a3e26c8
Augs 3 <110700696+august-3141@users.noreply.github.com>
2023-01-24 15:53:53 +0000

    removed os f removed os functions

--- Babel Searcher/babel-search.py
+++ Babel Searcher/babel-search.py
@@ -1,11 +1,9 @@
 import random
 import time
-import os
 
 inputWord = str(input("Enter a word to find: "))
 babelString, alphaString, count, = "", "a b c d e f g h i j k l m n o p q r s t u v w x y z", 0
 alphaString = alphaString.split()
-os.system('cls')
 
 start = time.time()
 while (inputWord in babelString) == False:
