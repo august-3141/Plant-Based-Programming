a39589bc61abfacaf94139e5ee9d7846ccf0c957
Augs 3 <110700696+august-3141@users.noreply.github.com>
2023-01-27 13:51:37 +0000

    Starting responses soon.

--- Language Processing/language_recognition.py
+++ Language Processing/language_recognition.py
@@ -6,8 +6,7 @@ nlp = spacy.load("en_core_web_sm")
 text = str(input("Enter a piece of text you want to analyze: "))
 doc = nlp(text)
 
-print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+nouns = [chunk.text for chunk in doc.noun_chunks]
+verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
 
-for entity in doc.ents:
-    print(entity.text, entity.label_)
+print(f"Nouns: {nouns}")
