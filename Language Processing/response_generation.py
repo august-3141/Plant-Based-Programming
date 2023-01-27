01deb2ae1c424ec5fac73f672f829b02bb47e2e9
August Harris <aug.harr3141>
2023-01-27 18:18:15 -0500

    Language Processing + Webscraping

--- Language Processing/response_generation.py
+++ Language Processing/response_generation.py
@@ -1,8 +1,9 @@
 # Samples Code and Library from https://spacy.io/
 
 import spacy
+from googlesearch import search 
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 text = str(input("Enter a piece of text you want to analyze: "))
 doc = nlp(text)
 
@@ -14,6 +15,8 @@ print(f"Nouns: {nouns}")
 if "?" in text:
 
     print(f"You asked a question about {nouns[-1]}:")
-    searchQ = nouns[-1].replace(" ", "_")
+    query = nouns[-1].replace(" ", "_")
 
-    print(f"https://en.wikipedia.org/wiki/{searchQ}")
+    for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+
+        print(f"Top Result: {j}")
